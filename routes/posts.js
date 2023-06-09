6c2f387289f49a94875c91c98e374cd5c51d2a1a
Wpstorm <wpstormdev@gmail.com>
2023-06-09 15:25:50 +0330

    feat: Added controllers and models for getPosts and createPost.

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,10 @@
 import express from "express";
+import {getPosts, createPost} from "../controllers/posts.js";
 
 const router = express.Router();
 
-router.get('/' ,(req,res)=>{
-    res.send('This worked!')
-})
+router.get('/' , getPosts);
+router.post('/' , createPost);
+router.get('/' , getPosts);
 
 export default router;
